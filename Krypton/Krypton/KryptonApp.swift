44ad9784f72474dc82b675a093932c3cca57f976
Marco Margarucci <margarucci.marco@gmail.com>
2023-09-08 15:32:26 +0200

    Initial commit

---
+++ Krypton/Krypton/KryptonApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptonApp.swift
+//  Krypton
+//
+//  Created by Marco Margarucci on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct KryptonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
