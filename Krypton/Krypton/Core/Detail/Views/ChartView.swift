746083ef6c12148814573e651253713741d0822e
Marco Margarucci <margarucci.marco@gmail.com>
2023-09-20 11:17:30 +0200

    Removed chart line opacity

--- Krypton/Krypton/Core/Detail/Views/ChartView.swift
+++ Krypton/Krypton/Core/Detail/Views/ChartView.swift
@@ -68,7 +68,7 @@ extension ChartView {
             }
             .trim(from: 0, to: percentage)
             .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
-            .shadow(color: lineColor.opacity(0.5), radius: 10, x: 0.0, y: 10)
+            .shadow(color: lineColor, radius: 10, x: 0.0, y: 10)
         }
     }
     
