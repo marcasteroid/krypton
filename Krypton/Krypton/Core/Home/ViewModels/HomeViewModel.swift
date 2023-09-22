e1330dcd1bec52a8e11595334ce0ffb3b4bba4d9
Marco Margarucci <margarucci.marco@gmail.com>
2023-09-22 14:36:40 +0200

    Fix percentage change

--- Krypton/Krypton/Core/Home/ViewModels/HomeViewModel.swift
+++ Krypton/Krypton/Core/Home/ViewModels/HomeViewModel.swift
@@ -138,7 +138,7 @@ class HomeViewModel: ObservableObject {
                 return previousValue
             }
             .reduce(0, +)
-        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        let percentageChange = ((portfolioValue - previousValue) / previousValue)
         let portfolio = Statistic(title: StatisticViewConstants.statisticPortfolioValueHeaderTitle,
                                   value: portfolioValue.asCurrencyWith2Decimals(),
                                   percentageChange: percentageChange)
